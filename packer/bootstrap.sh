3f9194db518420ebb5b2e3ea3911808f0c8fe791
kendall libby <klibby@mozilla.com>
2015-02-20 10:44:30 -0500

    ignore vagrant .box files

---
+++ packer/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+function log {
+    # todo: log to file?
+    STAMP=`date '+%F %T %z'`
+    echo "${STAMP} BOOTSTRAP: ${@}"
+}
+
+log "start"
+log "install puppet repo"
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+dpkg -i puppetlabs-release-precise.deb
+
+log "apt-get update"
+apt-get -y update
+
+log "apt-get upgrade"
+apt-get -y dist-upgrade
+
+log "apt-get install puppet"
+app-get -y install puppet
+
+log "end"
