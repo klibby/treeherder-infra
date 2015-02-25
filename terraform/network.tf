2d04eb5a73153597b9a6629cfe4e9e3f7396f86d
kendall libby <klibby@mozilla.com>
2015-02-25 10:45:41 -0500

    add tags

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,6 +4,14 @@ resource "aws_subnet" "public_subnet" {
     cidr_block = "${var.subnet_cidr_block}"
     availability_zone = "${var.subnet_az}"
     map_public_ip_on_launch = true
+    tags {
+        Name = "treeherder-subnet"
+        App = "treeherder"
+        Type = "subnet"
+        Env = "test"
+        Stack = "treeherder-aws-test"
+        Owner = "fubar"
+    }
 }
 
 # for private subnet, replace gateway_id with instance_id of NAT instance
@@ -13,10 +21,17 @@ resource "aws_route_table" "public_route_table" {
         cidr_block = "0.0.0.0/0"
         gateway_id = "${var.aws_igw_id}"
     }
+    tags {
+        Name = "treeherder-rt"
+        App = "treeherder"
+        Type = "rt"
+        Env = "test"
+        Stack = "treeherder-aws-test"
+        Owner = "fubar"
+    }
 }
 
 resource "aws_route_table_association" "public_route_table_association" {
     subnet_id = "${aws_subnet.public_subnet.id}"
     route_table_id = "${aws_route_table.public_route_table.id}"
 }
-
