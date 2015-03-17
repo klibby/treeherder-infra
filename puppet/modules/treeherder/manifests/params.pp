c84cc20313e84b5468e1176da3e1243c0c7a8924
kendall libby <klibby@mozilla.com>
2015-03-17 15:40:30 -0400

    add placeholders from IT puppet

--- puppet/modules/treeherder/manifests/params.pp
+++ puppet/modules/treeherder/manifests/params.pp
@@ -5,4 +5,21 @@ class treeherder::params {
   $app_root  = '/data'
   $user      = 'treeherder'
   $group     = 'treeherder'
+  #$rabbitmq_host
+  #$rabbitmq_cluster_nodes
+  #$rabbitmq_user
+  #$rabbitmq_pass
+  #$rabbitmq_vhost
+  #$erlang_cookie
+  #$db_name
+  #$db_rw_host
+  #$db_rw_user
+  #$db_rw_pass
+  #$db_ro_host
+  #$db_ro_user
+  #$db_ro_pass
+  #$web_nodes
+  #$django_secret
+  #$pulse_user
+  #$pulse_pass
 }
