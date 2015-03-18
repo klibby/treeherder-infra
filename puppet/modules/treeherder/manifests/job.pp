d93342100c74daadca352066e8cf246cd3fc5c3e
kendall libby <klibby@mozilla.com>
2015-03-18 15:00:44 -0400

    flip enable/ensure on jobs

--- puppet/modules/treeherder/manifests/job.pp
+++ puppet/modules/treeherder/manifests/job.pp
@@ -1,4 +1,6 @@
 define treeherder::job (
+  $enable     = "${treeherder::enable}",
+  $ensure     = "${treeherder::ensure}",
   $cmd_path   = "${treeherder::app_root}/treeherder-service/bin",
   $jobuser    = "${treeherder::user}",
   $app_dir    = "${treeherder::app_root}",
@@ -9,8 +11,8 @@ define treeherder::job (
   supervisord::program {
     "${name}":
       command        => "${cmd_path}/${name}",
-      ensure         => "${treeherder::enable}",
-      ensure_process => "${treeherder::ensure}",
+      ensure         => "${ensure}",
+      ensure_process => "${enable}",
       user           => "${jobuser}",
       directory      => "${app_dir}",
       stopsignal     => "${stopsignal}",
